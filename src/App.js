7469e81718198398157d54e07062fae22757f6c8
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-20 22:39:17 +0530

    Added padding -x

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App bg-gradient-to-br min-h-screen from-purple-800 to-purple-500 flex flex-col pt-20 items-center">
+    <div className="App bg-gradient-to-br min-h-screen from-purple-800 to-purple-500 flex flex-col pt-20 items-center px-10">
       <TodoList/>
     </div>
   );
