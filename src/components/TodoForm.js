d067417cc70a504ae7d5854f0175ee7f78c2d8a6
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-20 19:29:42 +0530

    Added addTodo logic

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-function TodoForm() {
+function TodoForm({onSubmit}) {
     const [input,setInput]=useState("");
 
     const handleChange=(e)=>{
@@ -11,6 +11,10 @@ function TodoForm() {
         e.preventDefault();
 
         // TODO:Add logic to add the todo
+        onSubmit({
+            id:crypto.randomUUID(),
+            text:input
+        })
         
         // emptying the input field after adding the Todo
         setInput("");
