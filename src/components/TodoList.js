fecf02b8ab430ec670a889686add024013f115f2
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-20 18:38:39 +0530

    Update TodoList.js
    Added addTodo function which appends the new todo at the beginning of the list using spread operator

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,11 +4,20 @@ import TodoForm from './TodoForm'
 function TodoList() {
     const [todos,setTodos]=useState([]);
 
+    const addTodo = (todo)=>{
+        // if text is empty then return
+        if(!todo.text || /^\s*$/.test(todo.text)){
+            return;
+        }
+
+        const newTodos=[todo,...todos];
+        setTodos(newTodos);
+    }
 
   return (
     <div className='flex flex-col'>
         <h1>Whats the plan for today</h1>
-        <TodoForm  />
+        <TodoForm  onSubmit={addTodo} />
     </div>
   )
 }
