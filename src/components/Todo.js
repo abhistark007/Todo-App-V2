8638f40e30fb821798478dce92cafb15bd524459
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-20 20:31:13 +0530

    Added edit & remove todo functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,20 +3,37 @@ import TodoForm from './TodoForm'
 import {RiCloseCircleLine} from 'react-icons/ri'
 import {AiFillEdit} from 'react-icons/ai'
 
-function Todo() {
+function Todo({todos,completeTodo,removeTodo,updateTodo}) {
     const [edit,setEdit]=useState({
         id:null,
         value:""
     });
 
+    const submitTodo=(value)=>{
+        updateTodo(edit.id,value);
+        setEdit({
+            id:null,
+            value:""
+        })
+    }
+
+
+    if(edit.id){
+        return <TodoForm onSubmit={submitTodo}/>
+    }
+
  return todos.map((todo,index)=>(
-    <div className={"flex"+todo.isComplete?'line-through':""} key={index}>
+    <div className={`flex ${todo.isComplete && "line-through"}`} key={index}>
         <div key={todo.id} onClick={()=>completeTodo(todo.id)}>
             {todo.text}
         </div>
         <div className='flex'>
-            <RiCloseCircleLine/>
-            <AiFillEdit/>
+            <RiCloseCircleLine
+            onClick={()=>removeTodo(todo.id)}
+            />
+            <AiFillEdit
+            onClick={()=>setEdit({id:todo.id,value:todo.text})}
+            />
         </div>
     </div>
  ))
