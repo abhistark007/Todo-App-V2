c730219e3c7f74bb01e364ec28c4edb4c068b3b9
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-20 18:10:28 +0530

    Added handleChange to be able to type in inputfield

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react'
 function TodoForm() {
     const [input,setInput]=useState("");
 
+    const handleChange=(e)=>{
+        setInput(e.target.value);
+    }
+
     const handleSubmit=(e)=>{
         e.preventDefault();
 
@@ -10,7 +14,7 @@ function TodoForm() {
 
   return (
     <form className='flex' onSubmit={handleSubmit}>
-        <input placeholder='Add a Todo' type='text' value={input} />
+        <input placeholder='Add a Todo' type='text' value={input} onChange={handleChange}/>
         <button >Add Todo</button>
     </form>
   )
