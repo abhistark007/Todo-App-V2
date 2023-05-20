26acdf9faf59d2cecc1111e7ee4047b5a2ce6d84
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-20 18:05:54 +0530

    Made sure the page don't refresh after submitting

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,8 +3,13 @@ import React, { useState } from 'react'
 function TodoForm() {
     const [input,setInput]=useState("");
 
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+
+    }
+
   return (
-    <form className='flex'>
+    <form className='flex' onSubmit={handleSubmit}>
         <input placeholder='Add a Todo' type='text' value={input} />
         <button >Add Todo</button>
     </form>
